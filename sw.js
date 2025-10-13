306f52a4573972b2b2acfa3685c4a6f913289075
KNODRON <juan.jimenezv08@gmail.com>
2025-10-13 17:15:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,5 @@
-const CACHE_NAME = 'pwa-fiscalias-v2';
+const CACHE_NAME = 'pwa-fiscalias-v3';
 const FILES_TO_CACHE = ['/', '/index.html', '/manifest.json', '/styles.css', '/app.js'];
-
-self.addEventListener('install', (evt) => {
-  evt.waitUntil(caches.open(CACHE_NAME).then((cache)=> cache.addAll(FILES_TO_CACHE)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (evt)=>{
-  evt.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', (evt)=>{
-  evt.respondWith(caches.match(evt.request).then(resp => resp || fetch(evt.request)));
-});
+self.addEventListener('install', (evt) => { evt.waitUntil(caches.open(CACHE_NAME).then((cache)=> cache.addAll(FILES_TO_CACHE))); self.skipWaiting();});
+self.addEventListener('activate', (evt)=>{ evt.waitUntil(clients.claim());});
+self.addEventListener('fetch', (evt)=>{ evt.respondWith(caches.match(evt.request).then(resp => resp || fetch(evt.request)));});
