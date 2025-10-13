cd41bade5796b91fdfc1877d10ff2b5393cf3994
KNODRON <juan.jimenezv08@gmail.com>
2025-10-13 17:45:43 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
 const CACHE_NAME = 'pwa-fiscalias-v3';
-const FILES_TO_CACHE = ['/', '/index.html', '/manifest.json', '/styles.css', '/app.js'];
-self.addEventListener('install', (evt) => { evt.waitUntil(caches.open(CACHE_NAME).then((cache)=> cache.addAll(FILES_TO_CACHE))); self.skipWaiting();});
-self.addEventListener('activate', (evt)=>{ evt.waitUntil(clients.claim());});
-self.addEventListener('fetch', (evt)=>{ evt.respondWith(caches.match(evt.request).then(resp => resp || fetch(evt.request)));});
+const FILES_TO_CACHE = ['index.html', 'styles.css', 'app.js', 'manifest.json'];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(caches.open(CACHE_NAME).then((cache)=> cache.addAll(FILES_TO_CACHE)).catch(()=>{}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt)=>{ evt.waitUntil(clients.claim()); });
+
+self.addEventListener('fetch', (evt)=>{
+  evt.respondWith(caches.match(evt.request).then(resp => resp || fetch(evt.request).catch(()=>resp)));
+});
